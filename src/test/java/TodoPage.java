fc18fd96f8c53b59b55afc9989a45228cfce88c9
samflynn98 <sam.flynn98@outlook.com>
2026-05-28 13:34:59 +0100

    modify, add & delete added to POM

--- src/test/java/TodoPage.java
+++ src/test/java/TodoPage.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import java.util.Random;
 
 public class TodoPage {
@@ -21,16 +22,34 @@ public class TodoPage {
         searchBar.sendKeys(Keys.ENTER);
     }
 
-    public void addMultipleItems(int itemNumber) {
+    public void addMultipleItems(int numberOfItems) {
         String[] words = {"apple", "banana", "orange", "grape", "melon"};
         Random random = new Random();
         WebElement searchBar = driver.findElement(By.id("todo-input"));
         int i = 1;
-        while (i <= itemNumber) {
+        while (i <= numberOfItems) {
             String randomWord = words[random.nextInt(words.length)];
             searchBar.sendKeys(randomWord);
             searchBar.sendKeys(Keys.ENTER);
             i++;
         }
+    }
+    public void modifyItem(int itemID, String textToAdd) {
+            WebElement itemToModify = driver.findElement(By.cssSelector("li:nth-child(" + itemID + ") label"));
+            new Actions(driver)
+                    .doubleClick(itemToModify)
+                    .perform();
+            WebElement textToModify = driver.findElement(By.cssSelector(".input-container:nth-child(1) > #todo-input"));
+            textToModify.sendKeys(textToAdd);
+            textToModify.sendKeys(Keys.ENTER);
+    }
+    public void deleteItem(int itemID, String textToAdd) {
+        WebElement itemToModify = driver.findElement(By.cssSelector("li:nth-child(" + itemID + ") label"));
+        new Actions(driver)
+                .doubleClick(itemToModify)
+                .perform();
+        WebElement textToModify = driver.findElement(By.cssSelector(".input-container:nth-child(1) > #todo-input"));
+        textToModify.sendKeys(textToAdd);
+        textToModify.sendKeys(Keys.ENTER);
     }
 }
